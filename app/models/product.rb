618e84cec505fc79647a9207740441cd41f3211a
Enrico Zschemisch <enrico@vicampus.com>
2016-12-14 03:02:31 -0500

    added more model tests

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,7 +1,7 @@
 class Product < ApplicationRecord
-  validates :title, presence: true, uniqueness: true
-  validates :description, presence: true
-  validates :image_url, presence: true, allow_blank: true, format: {
+  validates :title, presence: true, uniqueness: true, length: { minimum: 5 }
+  validates :description, presence: true, length: { minimum: 20 }
+  validates :image_url, format: {
     with: %r{\.(gif|jpg|png)\Z}i,
     message: 'must be a URL for a gif, jpg or png image.'
   }
--- test/controllers/products_controller_test.rb
+++ test/controllers/products_controller_test.rb
@@ -23,7 +23,6 @@ class ProductsControllerTest < ActionDispatch::IntegrationTest
 
   test "should create product" do
     assert_difference('Product.count') do
-      # post products_url, params: { product: { description: @product.description, image_url: @product.image_url, price: @product.price, title: @product.title } }
       post products_url, params: { product: @update }
     end
 
@@ -41,7 +40,6 @@ class ProductsControllerTest < ActionDispatch::IntegrationTest
   end
 
   test "should update product" do
-    # patch product_url(@product), params: { product: { description: @product.description, image_url: @product.image_url, price: @product.price, title: @product.title } }
     patch product_url(@product), params: { product: @update }
     assert_redirected_to product_url(@product)
   end
